c470d5c3dc22de5134bd70c640f35d3e69cce240
christian-bromann <contact@christian-bromann.com>
2014-04-27 21:09:48 +0200

    first commit - last commit ^^

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'vendor/dg/twitter-php/src/twitter.class.php';
+
+$string = file_get_contents('http://www.berlin.de/rubrik/hauptstadt/verbannte_buecher/verbannte-buecher.json');
+$data   = json_decode($string,true);
+
+$tweet = $data[rand(0,count($data))]['ocrResult'];
+$twitter = new Twitter('***************', '***************', '***************', '***************');
+
+$twitter->send($tweet);
+
+?>
